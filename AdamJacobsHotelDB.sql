0ef6901c7359bf551473142db056bd1d60149a40
Adam Jacobs <adam.jacobs.100@gmail.com>
2021-11-01 20:05:29 +0000

    001: database folders created

---
+++ AdamJacobsHotelDB.sql
@@ -0,0 +1,45 @@
+CREATE
+OR replace database AdamJacobsHotelDB;
+USE AdamJacobsHotelDB;
+CREATE
+OR replace TABLE roomType (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  roomType ENUM("single", "double", "suite"),
+  capacity INT,
+  microwave BOOLEAN,
+  refrigirator BOOLEAN,
+  kitchen BOOLEAN,
+  livingRoom BOOLEAN,
+  ADA BOOLEAN,
+  jacuzzi BOOLEAN,
+  kingBed INT,
+  queenBed INT
+);
+CREATE
+OR replace TABLE guests (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  guestName varchar(50),
+  guestAddress varchar(100),
+  city varchar(30),
+  county varchar(30),
+  postcode varchar(8),
+  phone varchar(15)
+);
+CREATE
+or replace table rooms (
+  roomNumber int not NULL PRIMARY KEY,
+  roomTypeID int,
+  foreign KEY (roomTypeID) references roomType(id)
+);
+CREATE
+or replace TABLE reservations (
+  id int PRIMARY KEY AUTO_INCREMENT not NULL,
+  roomNumber int,
+  guestID int,
+  adults int,
+  children int,
+  startDate date,
+  endDate date,
+  foreign key (roomNumber) references rooms(roomNumber),
+  foreign key (guestID) references guests(id)
+);
